86edcbad4715a24f5e2eee8c7619097c8301cbe4
Erictomm2002 <77874036+Erictomm2002@users.noreply.github.com>
2023-06-08 05:47:39 +0700

    Update PhongBanModel.php

--- Models/QuanLyHoSoModels/PhongBanModel.php
+++ Models/QuanLyHoSoModels/PhongBanModel.php
@@ -1,15 +1,14 @@
 <?php
     class PhongBanModel{
         function __construct($consetup){
-            $this->host = $consetup->host;
-            $this->user = $consetup->user;
-            $this->pass =  $consetup->pass;
-            $this->db = $consetup->db;
+            $this->hostname = $consetup->hostname;
+            $this->username = $consetup->username;
+            $this->password =  $consetup->password;
+            $this->database = $consetup->database;
         }
-
         public function open_db()
         {
-            $this->condb=new mysqli($this->host,$this->user,$this->pass,$this->db);
+            $this->condb=new mysqli($this->hostname,$this->username,$this->password,$this->database);
             if ($this->condb->connect_error)
             {
                 die("Erron in connection: " . $this->condb->connect_error);
@@ -29,11 +28,11 @@
                 $this->open_db();
                 if($id>0)
                 {
-                    $query=$this->condb->prepare("SELECT * FROM phongban WHERE maPB=?");
+                    $query=$this->condb->prepare("SELECT * FROM phong_ban WHERE maPb=?");
                     $query->bind_param("s",$id);
                 }
                 else
-                {$query=$this->condb->prepare("SELECT * FROM phongban");    }
+                {$query=$this->condb->prepare("SELECT * FROM phong_ban");    }
 
                 $query->execute();
                 $res=$query->get_result();
